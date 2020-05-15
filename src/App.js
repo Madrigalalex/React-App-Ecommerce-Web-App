6964646455ae1201fc531097a155a17952680120
Madrigalalex <azm0208@tigermail.auburn.edu>
2020-05-14 23:51:03 -0700

    added and changed the name of js file card to cart

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Components/Navbar';
+import ProductList from './Components/ProductList';
+import Details from './Components/Details';
+import Cart from './Components/Cart';
+import Product from './Components/Product';
+import Default from './Components/Default';
 
 
 function App() {
   return (
     <React.Fragment>
-    
+      <Navbar>
+        
+      </Navbar>
     </React.Fragment>
   );
 }
--- src/Components/Default.js
+++ src/Components/Default.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-export default class extends Component {
+export default class Default extends Component {
     render() {
         return (
             <div>
-                <h3>Hello from card</h3>
+                <h3>Page not found</h3>
             </div>
         )
     }

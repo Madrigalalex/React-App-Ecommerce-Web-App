526dc15f621f71fe076f94bd213adc94a2307848
Madrigalalex <azm0208@tigermail.auburn.edu>
2020-05-14 23:39:26 -0700

    adding components to the project.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 function App() {
   return (
-   <div className="container">
-    <div className="row">
-        <div className="col-6">column number one</div>
-        <div className="col-6">column number two</div>
-    </div>
-   </div>
+    <React.Fragment>
+    
+    </React.Fragment>
   );
 }
 

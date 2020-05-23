7bc6bcd092e22367366b1f69809499a4eb5191ae
Madrigalalex <azm0208@tigermail.auburn.edu>
2020-05-23 00:17:27 -0700

    made a button component and added it to the navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
 import logo from '../logo.svg';
+import styled from  'styled-components';
+import {ButtonContainer} from './Button';
 
 
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
                 <Link to='/'> 
                     <img src={logo} alt="store" className="navbar-brand"/>
                  </Link>
@@ -18,12 +20,26 @@ export default class Navbar extends Component {
                     </li>
                  </ul>
                  <Link to="/cart" className="ml-auto">
-                    <button>
-                        <i className="fas fa-cart-plus"/>
+                    <ButtonContainer>
+                        <span className="mr-2">
+                            <i className="fas fa-cart-plus"/>
+                        </span>
                         My Cart
-                    </button>
+                    </ButtonContainer>
                  </Link>
-            </nav>
+            </NavWrapper>
         )
     }
 }
+
+const NavWrapper = styled.nav`
+    background: #000000;
+    .nav-link{
+        color:var(--mainWhite)!important;
+        font-size:2.3rem;
+        text-transfom:capitalize!important;
+        
+    }
+    
+`;
+

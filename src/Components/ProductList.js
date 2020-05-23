24d4478108f60025d64f2100da90532b5dc5cc68
Madrigalalex <azm0208@tigermail.auburn.edu>
2020-05-23 11:04:11 -0700

    imported data and added a title to product list

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
 import Product from './Product';
+import Title from './Title';
+import {storeProduct} from '../data';
 
 export default class ProductList extends Component {
+    state = {
+        products:[]
+    }
     render() {
         return (
-            <div>
-                <h3>hello from product list</h3>
-            </div>
+            <React.Fragment>
+                <div className="dy-5">
+                    <div className="container">
+                        <Title name="Phone" title="Products"/>
+                        <div className="row">
+
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+            //<Product/>
         )
     }
 }

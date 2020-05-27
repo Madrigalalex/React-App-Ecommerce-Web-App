64f1d12d5b2d1f2dffa352398ee2fc1534a9453c
Madrigalalex <azm0208@tigermail.auburn.edu>
2020-05-27 00:24:24 -0700

    added more code

--- src/context.js
+++ src/context.js
@@ -12,7 +12,8 @@ class ProductProvider extends Component {
 
     state={
         products:[],
-        detailProduct:detailProduct
+        detailProduct:detailProduct,
+        cart:[]
     }
 
     componentDidMount() {
@@ -34,12 +35,27 @@ class ProductProvider extends Component {
         return product;
     }
 
-    handleDetail = () => {
-        console.log("hello from detail");
+    handleDetail = (id) => {
+        const product = this.getItem(id);
+        this.setState(()=>{
+            return {detailProduct:product}
+        })
     }
     addToCart = (id) => {
-        console.log(`hello from add to cart.id is ${id}`);
-    }
+        let tempProducts = [...this.state.products];
+        const index = tempProducts.indexOf(this.getItem(id));
+        const product = tempProducts[index];
+        product.inCart = true;
+        product.count = 1;
+        const price = product.price;
+        product.total = price;
+        this.setState(() => {
+            return {products : tempProducts, cart:[...this.state.cart, product]};
+        }, 
+        () => {
+            console.log(this.state);
+        });
+    };
 
     render() {
         return (

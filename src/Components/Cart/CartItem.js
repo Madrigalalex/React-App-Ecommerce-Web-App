8e36013e2c052638579875d993cb9557fb352ed1
Madrigalalex <azm0208@tigermail.auburn.edu>
2020-06-03 22:26:05 -0700

    added columns for cart page

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
-export default function CartItem() {
+export default function CartItem({item, value}) {
+    const {id,title,img,price,total,count} = item;
+    const {increment, decrement, removeItem} = value;
     return (
-        <div>
-            This is a cart item
+        <div className="row my-1 text-capitalize text-center">
+            {/**First Column */}
+            <div className="col-10 mx-auto col-lg-2">
+                <img src={img} style={{width: '5rem', height: "5rem"}} className="img-fluid" alt="product"/>
+            </div>
+
+            {/**Second Column */}
+            <div className="col-10 mx-auto col-lg-2">
+                <span className="d-lg-none">
+                    Product:
+                </span>
+                    {title}
+            </div>
+
+            {/**Third Column */}
+            <div className="col-10 mx-auto col-lg-2">
+   
+            </div>
+
+            {/**Fourth Column */}
+            <div className="col-10 mx-auto col-lg-2">
+   
+            </div>
+
+
         </div>
     )
 }

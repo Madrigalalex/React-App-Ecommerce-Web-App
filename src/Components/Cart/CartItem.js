ff1013aaa072f669a41bef15b1f27aa08a11412f
Madrigalalex <azm0208@tigermail.auburn.edu>
2020-06-05 23:28:26 -0700

    added final files and functionality to the app

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -4,7 +4,8 @@ export default function CartItem({item, value}) {
     const {id,title,img,price,total,count} = item;
     const {increment, decrement, removeItem} = value;
     return (
-        <div className="row my-1 text-capitalize text-center">
+        <div className="row my-2 text-capitalize text-center">
+
             {/**First Column */}
             <div className="col-10 mx-auto col-lg-2">
                 <img src={img} style={{width: '5rem', height: "5rem"}} className="img-fluid" alt="product"/>
@@ -29,9 +30,23 @@ export default function CartItem({item, value}) {
             {/**Fourth button Column */}
             <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
                 <div className = "d-flex justify-content-center">
-                    <span className="btn btn-black mx-1" onClick={()=>decrement(id)}></span>
+                    <span className="btn btn-black mx-1" onClick={()=>decrement(id)}> - </span>
+                    <span className="btn btn-black mx-1">{count}</span>
+                    <span className="btn btn-black mx-1" onClick={()=>increment(id)}> + </span>
 
                 </div>
+            </div>
+
+            {/**Fifth column*/}
+            <div className="col-10 mx-auto col-lg-2">
+                <div className="cart-icon" onClick={()=>removeItem(id)}>
+                    <i className="fas fa-trash"></i>
+                </div>
+            </div>
+
+            {/**Sixth column*/}
+            <div className="col-10 mx-auto col-lg-2">
+                <strong> Item Total : ${total} </strong>
             </div>
 
 
